c52aebc6473df333d9c3ba6990bdd10d4cac23f8
Liem Le <liem.lv@techchain.vn>
2018-12-14 22:07:10 +0700

    init

--- app/Models/Traits/HasPermissionsTrait.php
+++ app/Models/Traits/HasPermissionsTrait.php
@@ -8,8 +8,30 @@
 
 namespace App\Models\Traits;
 
+use App\Models\Role;
 
-class HasPermissionsTrait
+trait HasPermissionsTrait
 {
+    public function roles() {
+        return $this->belongsToMany(Role::class,'users_roles');
 
+    }
+
+    public function hasRole(...$roles) {
+        foreach ($roles as $role) {
+            if($this->roles->contains('slug',$role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function hasPermissionThroughRole($permission) {
+        foreach ($permission->roles as $role){
+            if($this->roles->contains($role)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
